b56e201dc0051cf87dc81e66e15aaa0965bcbf27
syhan <oboznyy.maks99@gmail.com>
2025-05-08 20:32:34 +0700

    add NetworkResponse interface for monitoring network requests

---
+++ app/src/main/java/com/syhan/pokeapiclient/common/data/NetworkResponse.kt
@@ -0,0 +1,12 @@
+package com.syhan.pokeapiclient.common.data
+
+sealed interface NetworkResponse {
+    data object Loading: NetworkResponse
+    data class Error(val type: NetworkErrorType): NetworkResponse
+    data object Success: NetworkResponse
+}
+
+enum class NetworkErrorType {
+    NoInternetError,
+    UnexpectedError,
+}
