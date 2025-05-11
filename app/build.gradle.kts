005b0879f63f0d37a3a8ba93c7804662c0eeedaf
syhan <oboznyy.maks99@gmail.com>
2025-05-11 19:51:13 +0700

    add room and ksp dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -65,6 +66,7 @@ dependencies {
     implementation(libs.coil.okhttp)
     implementation(libs.androidx.paging)
     implementation(libs.androidx.paging.compose)
+    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
