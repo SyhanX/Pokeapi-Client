314030b1859718822cba0bd11cc42edcd015e1cb
syhan <oboznyy.maks99@gmail.com>
2025-05-14 17:30:26 +0700

    remove unused class

--- app/src/main/java/com/syhan/pokeapiclient/feature_pokemon_search/domain/model/PokemonDetails.kt
+++ app/src/main/java/com/syhan/pokeapiclient/feature_pokemon_search/domain/model/PokemonDetails.kt
@@ -5,16 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Immutable
 @Serializable
-data class PokemonDetailsShort(
-    val id: Int,
-    val name: String,
-    val sprites: Sprites,
-    val types: List<Type>,
-    val stats: List<Stat>
-)
-
-@Immutable
-@Serializable
 data class PokemonDetails(
     val id: Int,
     val name: String,
